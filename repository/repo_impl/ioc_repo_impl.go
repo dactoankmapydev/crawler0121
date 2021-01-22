0e4837e15d53b890bdbd47fd303174d216ddd1e2
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-22 14:48:31 +0700

    Update ioc_repo_impl.go

--- repository/repo_impl/ioc_repo_impl.go
+++ repository/repo_impl/ioc_repo_impl.go
@@ -2,7 +2,10 @@ package repo_impl
 
 import (
 	"context"
+	"errors"
+	"github.com/olivere/elastic/v7"
 	"ioc-provider/db"
+	"ioc-provider/model"
 	"ioc-provider/repository"
 	"log"
 )
@@ -17,235 +20,48 @@ func NewIocRepo(es *db.ElasticDB) repository.IocRepo {
 	}
 }
 
-func (i IocRepoImpl) CreateIndexPost(elasticIndexName string) {
+func (ioc IocRepoImpl) CreateIndex(index string, mapping string) error {
 	ctx := context.Background()
-	const mapping = `
-	{
-		"settings":{
-			"number_of_shards": 2,
-			"number_of_replicas": 0
-		},
-		"mappings":{
-			"properties":{
-				"pulse_id":{
-					"type":"text"
-				},
-				"name":{
-					"type":"text
-				},
-				"description":{
-					"type":"text"
-				},
-				"author_name":{
-					"type":"text"
-				},
-				"modified":{
-					"type":"date"
-				},
-				"created":{
-					"type":"date"
-				},
-				"targeted_countries":{
-					"type":"text"
-				},
-				"industries":{
-					"type":"text"
-				}
-				"malware_families":{
-					"type":"text"
-				},
-				"attack_ids":{
-					"type":"text"
-				},
-				"references":{
-					"type":"text"
-				},
-				"category":{
-					"type":"text"
-				}
-			}
+	exists, err := ioc.es.Client.IndexExists(index).Do(ctx)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		createIndex, err := ioc.es.Client.CreateIndex(index).
+			BodyString(mapping).
+			Do(ctx)
+		if err != nil {
+			return err
 		}
-	}`
+		if !createIndex.Acknowledged {
+			return errors.New("CreateIndex was not acknowledged. Check that timeout value is correct.")
+		}
+	}
+	return nil
+}
 
-	// Sử dụng dIndexExists để kiểm tra xem chỉ mục cụ thể có tồn tại hay không.
-	exists, err := i.es.Client.IndexExists(elasticIndexName).Do(ctx)
+func (ioc IocRepoImpl) Index(index string, id string, doc interface{}) error {
+	ctx := context.Background()
+	_, err := ioc.es.Client.Index().
+		Index(index).
+		Id(id).
+		BodyJson(doc).
+		Do(ctx)
 	if err != nil {
 		log.Println(err)
 	}
-	if !exists {
-		// Tạo một chỉ mục mới.
-		createIndex, err := i.es.Client.CreateIndex(elasticIndexName).BodyString(mapping).Do(ctx)
-		if err != nil {
-			log.Println(err)
-		}
-		if !createIndex.Acknowledged {}
-	}
-	return
+	return nil
 }
 
-func (i IocRepoImpl) CreateIndexIndicator(elasticIndexName string) {
+func (ioc IocRepoImpl) SearchIndex(index string, search string) error {
 	ctx := context.Background()
-	const mapping = `
-	{
-		"settings":{
-			"number_of_shards": 2,
-			"number_of_replicas": 0
-		},
-		"mappings":{
-			"properties":{
-				"ioc_id":{
-					"type":"text"
-				},
-				"ioc":{
-					"type":"text
-				},
-				"ioc_type":{
-					"type":"text"
-				},
-				"created_time":{
-					"type":"date"
-				},
-				"crawled_time":{
-					"type":"date"
-				},
-				"source":{
-					"type":"text"
-				},
-				"category":{
-					"type":"text"
-				}
-			}
-		}
-	}`
-
-	// Sử dụng dIndexExists để kiểm tra xem chỉ mục cụ thể có tồn tại hay không.
-	exists, err := i.es.Client.IndexExists(elasticIndexName).Do(ctx)
+	matchQuery := elastic.NewMatchQuery(model.AppName, search)
+	res, err := ioc.es.Client.Search(index).
+		Index(index).
+		Query(matchQuery).
+		Do(ctx)
 	if err != nil {
-		log.Println(err)
+		return err
 	}
-	if !exists {
-		// Tạo một chỉ mục mới.
-		createIndex, err := i.es.Client.CreateIndex(elasticIndexName).BodyString(mapping).Do(ctx)
-		if err != nil {
-			log.Println(err)
-		}
-		if !createIndex.Acknowledged {}
-	}
-	return
+    return nil
 }
-
-func (i IocRepoImpl) CreateIndexSample(elasticIndexName string){
-	ctx := context.Background()
-	const mapping = `
-	{
-		"settings":{
-			"number_of_shards": 2,
-			"number_of_replicas": 0
-		},
-		"mappings":{
-			"properties":{
-				"names":{
-					"type":"text"
-				},
-				"sha256":{
-					"type":"text
-				},
-				"sha1":{
-					"type":"text"
-				},
-				"md5":{
-					"type":"text"
-				},
-				"first_submit":{
-					"type":"date"
-				},
-				"notification_date":{
-					"type":"date"
-				},
-				"file_type":{
-					"type":"text"
-				},
-				"tags":{
-					"type":"text"
-				},
-				"engines_detected":{
-					"type":"text
-				},
-				"detected":{
-					"type":"integer"
-				},
-				"point":{
-					"type":"integer"
-				}
-			}
-		}
-	}`
-
-	// Sử dụng dIndexExists để kiểm tra xem chỉ mục cụ thể có tồn tại hay không.
-	exists, err := i.es.Client.IndexExists(elasticIndexName).Do(ctx)
-	if err != nil {
-		log.Println(err)
-	}
-	if !exists {
-		// Tạo một chỉ mục mới.
-		createIndex, err := i.es.Client.CreateIndex(elasticIndexName).BodyString(mapping).Do(ctx)
-		if err != nil {
-			log.Println(err)
-		}
-		if !createIndex.Acknowledged {}
-	}
-	return
-}
-
-func (i IocRepoImpl) CreateIndexCompromised(elasticIndexName string) {
-	ctx := context.Background()
-	const mapping = `
-	{
-		"settings":{
-			"number_of_shards": 2,
-			"number_of_replicas": 0
-		},
-		"mappings":{
-			"properties":{
-				"uid":{
-					"type":"text"
-				},
-				"hostname":{
-					"type":"text
-				},
-				"src":{
-					"type":"text"
-				},
-				"victim_hash":{
-					"type":"text"
-				},
-				"creation_date":{
-					"type":"date"
-				},
-				"timestamp":{
-					"type":"date"
-				},
-				"country":{
-					"type":"text"
-				}
-			}
-		}
-	}`
-
-	// Sử dụng dIndexExists để kiểm tra xem chỉ mục cụ thể có tồn tại hay không.
-	exists, err := i.es.Client.IndexExists(elasticIndexName).Do(ctx)
-	if err != nil {
-		log.Println(err)
-	}
-	if !exists {
-		// Tạo một chỉ mục mới.
-		createIndex, err := i.es.Client.CreateIndex(elasticIndexName).BodyString(mapping).Do(ctx)
-		if err != nil {
-			log.Println(err)
-		}
-		if !createIndex.Acknowledged {}
-	}
-	return
-}
-
-
