5d69ecc5640f5a93c968bdfef6c6570e31036f06
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-22 14:50:33 +0700

    Update es.go

--- db/es.go
+++ db/es.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/olivere/elastic/v7"
+	"time"
 )
 
 type ElasticDB struct {
@@ -12,14 +13,17 @@ type ElasticDB struct {
 	Port string
 }
 
-func (es *ElasticDB) NewElasticDB() {
-	client, err := elastic.NewClient()
-	if err != nil {
-		fmt.Printf("connection failed: %v\n", err)
-	}
-	info, code, err := client.Ping("http://" + es.Host + ":" + es.Port).Do(context.Background())
+func (es *ElasticDB) NewElasticDB() () {
+	es.Client, _ = elastic.NewClient(elastic.SetURL(fmt.Sprintf("http://%s:%s", es.Host, es.Port)),
+		elastic.SetSniff(false),
+		elastic.SetHealthcheck(false),
+		elastic.SetHealthcheckInterval(3*time.Second),
+	)
+
+	info, code, err := es.Client.Ping(fmt.Sprintf(`http://%s:%s`,es.Host,es.Port)).Do(context.Background())
 	if err != nil {
 		fmt.Printf("connection failed: %v\n", err)
 	}
 	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
+	//return es.Client, err
 }
