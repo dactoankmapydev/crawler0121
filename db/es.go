d5f3f2ac630e48dd1d977cedb9152b2a7edbcb9e
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-25 19:18:03 +0700

    Update es.go

--- db/es.go
+++ db/es.go
@@ -7,24 +7,23 @@ import (
 	"time"
 )
 
+var timeout = time.Duration(15 * time.Second)
+
 type ElasticDB struct {
 	Client *elastic.Client
 	Host   string
 	Port   string
 }
 
-func (es *ElasticDB) NewElasticDB() () {
-	for {
-		es.Client, _ = elastic.NewClient(elastic.SetURL(fmt.Sprintf("http://%s:%s", es.Host, es.Port)),
-			elastic.SetSniff(false),
-		)
-		info, code, err := es.Client.Ping(fmt.Sprintf(`http://%s:%s`, es.Host, es.Port)).Do(context.Background())
-		if err != nil {
-			fmt.Printf("connection failed: %v\n", err)
-			time.Sleep(3*time.Second)
-		} else {
-			fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
-			break
-		}
+func (es *ElasticDB) NewElasticDB() {
+	es.Client, _ = elastic.Dial(elastic.SetURL(fmt.Sprintf("http://%s:%s", es.Host, es.Port)),
+		elastic.SetSniff(false),
+		elastic.SetHealthcheck(false),
+	)
+
+	info, code, err := es.Client.Ping(fmt.Sprintf(`http://%s:%s`, es.Host, es.Port)).Do(context.Background())
+	if err != nil {
+		fmt.Printf("connection failed: %v\n", err)
 	}
+	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
 }
