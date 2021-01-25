71894547d2acf629d18e8d66bc203dbb9eca8691
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-25 19:17:34 +0700

    Update indicator.go

--- model/indicator.go
+++ model/indicator.go
@@ -1,10 +1,10 @@
 package model
 
 type Indicators struct {
-	IocID int64 `json:"id"`
-	Ioc string `json:"indicator"`
-	IocType string `json:"type"`
-	CreatedTime string `json:"created"`
+	IocID       int64    `json:"id"`
+	Ioc         string   `json:"indicator"`
+	IocType     string   `json:"type"`
+	CreatedTime string   `json:"created"`
 	CrawledTime string   `json:"crawled_time"`
 	Source      string   `json:"source"`
 	Category    []string `json:"category"`
