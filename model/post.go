d40295bfa540e1cbf02a390397fe5a1701b2775a
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-25 19:16:57 +0700

    Update post.go

--- model/post.go
+++ model/post.go
@@ -1,18 +1,16 @@
 package model
 
 type Post struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Description string `json:"description"`
-	AuthorName string `json:"author_name"`
-	Modified string `json:"modified"`
-	Created string `json:"created"`
-	Tags []string `json:"tags"`
+	ID                string   `json:"id"`
+	Name              string   `json:"name"`
+	Description       string   `json:"description"`
+	AuthorName        string   `json:"author_name"`
+	Modified          string   `json:"modified"`
+	Created           string   `json:"created"`
+	Tags              []string `json:"tags"`
 	TargetedCountries []string `json:"targeted_countries"`
-	MalwareFamilies []string `json:"malware_families"`
-	AttackIds []string `json:"attack_ids"`
-	References []string `json:"references"`
-	Industries []string `json:"industries"`
+	MalwareFamilies   []string `json:"malware_families"`
+	AttackIds         []string `json:"attack_ids"`
+	References        []string `json:"references"`
+	Industries        []string `json:"industries"`
 }
-
-
