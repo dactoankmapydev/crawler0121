7a2c36e39802b2adbfffb54413bb932cd2fac321
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-26 19:36:30 +0700

    Update ioc_repo.go

--- repository/ioc_repo.go
+++ repository/ioc_repo.go
@@ -1,8 +1,13 @@
 package repository
 
+import "ioc-provider/model"
+
 type IocRepo interface {
 	ExistsIndex(indexName string) bool
 	CreateIndex(indexName, mapping string)
 	InsertIndex(indexName, id string, doc interface{}) bool
 	ExistsDoc(indexName, id string) bool
+
+	InsertManyIndexIoc(indexName, id string, docs []model.Indicators) bool
+	InsertManyIndexPost(indexName, id string, docs []model.Post) bool
 }
